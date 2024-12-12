b979c594a3b5e130628401ce749033c3362497f0
hKriszti-svg <krisztinahorgos@yahoo.com>
2024-12-12 11:51:49 +0100

    changed java script ,added Ai

--- src/index.js
+++ src/index.js
@@ -20,14 +20,13 @@ function generatePoem(event) {
 let instructionsInput= document.querySelector("#user-instructions");
 let apiKey = "ae026d6933ff4e9b69c0f323co1d1d9t";
 let prompt = `User instructions:Generate a German poem about  ${instructionsInput.value}`;
-let context= "You are a funny Poem expert and love to write short poems. You mission is to generate a 4 line poem in basic HTML without saying  that is html  and separate each line with a <br />. Make sure to follow the user instructions. Do not include a title to the poem. Sign the poem with 'SheCodes AI' inside a <strong> element at the end of the poem and NOT at the beginning";
+let context= "You are a funny Poem expert and love to write short poems. You mission is to generate a 4 line poem in basic HTML without saying  that is html  and separate each line with a <br />. Make sure to follow the user instructions. Do not include a title to the poem. Sign the poem with 'SheCodes AI' inside a <strong> element at the end of the poem and NOT at the beginning.";
 let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
-console.log("Generating Poem ");
-console.log(`Prompt: ${prompt}`);
-console.log(`Context: ${context}`);
 
-
+let poemElement= document.querySelector("#poem");
+poemElement.classList.remove("hidden");
+poemElement.innerHTML = `<div class="generating" >⌛Generating a German poem about ${instructionsInput.value}</div>`;
 
 
 axios.get(apiURL).then(displayPoem);
